b20ea3547d882d2ad2341182d4027cd20eea1af8
Madhav Sharma <madhav032001@gmail.com>
2023-10-03 16:07:28 +0530

    second commit

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwwjq-1hbh7P-k3b8qqixpR6iO0qX3nEqK9jsjIbBn93s3XF2iUn2YTw4FWAYLzVnFC/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+    msg.innerHTML="Message Sent Successfully "
+    setTimeout(function(){
+        msg.innerHTML=""
+    },5000)
+    form.reset()
+})
+    .catch(error => console.error('Error!', error.message))
+})
